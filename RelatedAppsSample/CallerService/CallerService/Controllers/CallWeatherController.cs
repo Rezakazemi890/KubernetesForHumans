87d7a32604f2139c3a8ef91947ca29a42fcf0bc6
Rezakazemi890 <rezakazemi890@gmail.com>
2024-10-30 11:32:25 +0330

    feat: add related apps sample and guid

---
+++ RelatedAppsSample/CallerService/CallerService/Controllers/CallWeatherController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CallerService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CallWeatherController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public CallWeatherController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeather()
+        {
+            // Call the Weather API from the first service
+            var response = await _httpClient.GetStringAsync("http://weather-service/WeatherForecast");
+            return Ok(response);
+        }
+    }
+}
